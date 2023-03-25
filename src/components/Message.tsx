3d936cd2d80c6f760eee077981428f760b8a49a5
AB56 <alainbidault56@gmail.com>
2023-03-25 11:05:30 +0100

    modif  esthétiques

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import { MessageInt } from '../Model';
+import React from "react";
+import { MessageInt } from "../Model";
+import "../styles/message.scss";
+
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 
 type Props = {
-mess :MessageInt;
-messageData : MessageInt[];
-setMessageData:React.Dispatch<React.SetStateAction<MessageInt[]>>
-}
-const Message = ({mess , messageData, setMessageData} : Props) => {
-const formaterDate = (date:number) => {
-  return new Date(date).toLocaleDateString("FR-fr");
-}
+  mess: MessageInt;
+  messageData: MessageInt[];
+  setMessageData: React.Dispatch<React.SetStateAction<MessageInt[]>>;
+};
+const Message = ({ mess, messageData, setMessageData }: Props) => {
+  const formaterDate = (date: number) => {
+    return new Date(date).toLocaleDateString("FR-fr");
+  };
 
-const handleDelete = () => {
-  setMessageData( messageData.filter((el) => el.id !== mess.id))
-}
-
+  const handleDelete = () => {
+    setMessageData(messageData.filter((el) => el.id !== mess.id));
+  };
 
   return (
-   <div className="message-container">
-    <p>{formaterDate(mess.date)}<br></br> {" key "} {mess.id} <br></br>
-    {mess.nom}{' '} <br></br>{mess.message}
-    <span id='delete' onClick={() => handleDelete()}>&#10008;</span>
-    </p>
-    
-    
-
-   </div>
+    <div className="message-container">
+      <p />
+      {formaterDate(mess.date)}
+      <br></br> {" key "} {mess.id} <br />
+      {mess.nom} <br />
+      {mess.message}
+      <Tooltip title="Delete">
+        <IconButton onClick={() => handleDelete()}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+    </div>
   );
 };
 
