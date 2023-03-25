e242f3d4fab554846e9fa8c335c4085f34d3bcae
AB56 <alainbidault56@gmail.com>
2023-03-25 16:07:41 +0100

    messages d'erreurs

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,28 @@ import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
 
 const App = () => {
-  const [errors, setErrors] = useState(false);
+
   const [messageData, setMessageData] = useState<MessageInt[]>([]);
   const [isSubmitSuccessful, setIsSubmitSuccessful] = useState(false);
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // setError,
+    formState: { errors },
+    reset,
+  } = useForm();
 
   useEffect(() => {
     reset({
       leMessage: "",
       leNom: "",
     });
-    setErrors(false);
+   
   }, [isSubmitSuccessful, reset]);
 
-  const onError = (erreurs: any, e: any) => {
-    
-    erreurs ? console.log("erreurs ", errors) : console.log("ok");
-
+  const onError = (errors: any, e: any) => {
+    errors ? console.log("erreurs ", errors) : console.log("ok");
     setIsSubmitSuccessful(false);
   };
 
@@ -46,23 +51,37 @@ const App = () => {
       <h1>POSTER UN MESSAGE ...</h1>
       <form onSubmit={handleSubmit(onSubmit, onError)}>
         <input
-          type="name"
+          type="text"
           placeholder="Nom"
           id="name"
           className="name"
-          {...register("leNom", { required: true })}
+          {...register("leNom", 
+          
+          {  minLength: {
+              value: 3,
+              message: "⚠ 3 caractères minimum",
+            },
+            required : "Veuillez indiquer votre nom"}
+          )}
         />
-
+{errors.leNom && <i>{errors.leNom.message?.toString()}</i>}
         <textarea
           placeholder="Votre message"
           id="leMessage"
-          {...register("leMessage", { required: true, maxLength: 1000 })}
+          {...register("leMessage", { minLength :
+            { value:12, message:" 12 caractères minimum" },
+            required : "Veuillez rédiger votre message"
+          })}
         />
+{ errors.leMessage && <p>{errors.leMessage.message?.toString()}</p> }
+
         <Button type="submit" variant="contained" endIcon={<SendIcon />}>
           Envoyer
         </Button>
         <br></br>
       </form>
+
+      
       <div className="list-message">
         <h2>Liste des messages</h2>
         <div>
