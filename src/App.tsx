d117a5d8de7211d8a94f1e3b5484e6c3ca2fd9b2
AB56 <alainbidault56@gmail.com>
2023-03-25 08:02:55 +0100

    première

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import Message from "./components/Message";
+import { MessageInt } from "./Model";
+import "../src/App.css";
 
-function App() {
+const App = () => {
+  const inputMessage = useRef<HTMLInputElement>(null);
+  const inputNom = useRef<HTMLInputElement>(null);
+  const [messageData, setMessageData] = useState<MessageInt[]>([]);
+
+  const handleSumit = (e: any) => {
+    e.preventDefault();
+    if (inputMessage) {
+      const mess: MessageInt = {
+        id: Math.round(Math.random() * Date.now()),
+        nom: inputNom.current?.value,
+        message: inputMessage.current?.value.toString(),
+        date: Date.now(),
+      };
+     
+       setMessageData((messageData) => [...messageData, mess]);
+      //  setMessageData((dataActuelles) => [...dataActuelles, mess]);
+    }
+    (document.getElementById("inputMessage") as HTMLInputElement).value = "";
+    (document.getElementById("name") as HTMLInputElement).value = "";
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <div>
+      <h1>POSTER UN MESSAGE</h1>
+      <form
+        onSubmit={(e) => {
+          handleSumit(e);
+        }}
+      >
+        <input type="nom" placeholder="Nom" id="name" ref={inputNom} />
+        <input
+          type="text"
+          placeholder="Votre message"
+          id="inputMessage"
+          ref={inputMessage}
+        />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <h2>Liste des messages</h2>
+      <div> { messageData?.map((lesMess) => 
+      (<Message mess={lesMess} messageData={messageData}
+       setMessageData={setMessageData}
+        key={lesMess.id}/>)) }
+      </div> 
+  </div>
+ 
+);
+      };
 export default App;
